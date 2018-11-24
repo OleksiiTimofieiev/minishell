6b0afc65f707a18d16b1e5a40bd81251be8a3d13
contrbas@gmail.com <K4rmU78B>
2018-11-24 21:53:00 +0200

    started to work on the cd function

--- srcs/cd.c
+++ srcs/cd.c
@@ -6,17 +6,23 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/24 13:28:48 by otimofie          #+#    #+#             */
-/*   Updated: 2018/11/24 20:54:52 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/11/24 21:52:22 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-// manage cd $ENVVAR
 // 	2. parameter can not be accessed;
-// add the whole path
 
-static int		len_2d_array(char **array) // to libft
+void		ft_clean(char **envp)
+{
+	if (envp[6])
+		free(envp[6]);
+	if (envp[22])
+		free(envp[22]);
+}
+
+static int	len_2d_array(char **array)
 {
 	short i;
 
@@ -30,21 +36,16 @@ short	cd_main(char *pwd_new, char *pwd_old, char **envp, char **command_line)
 {
 	if (chdir(&pwd_new[4]) == 0)
 	{
-		if (envp[6])
-			free(envp[6]);
-		if (envp[22])
-			free(envp[22]);
+		ft_clean(envp);
 		envp[6] = ft_strdup(pwd_new);
 		envp[22] = ft_strdup(pwd_old);
-		
 		return (1);
 	}
 	else
 	{
 		ft_printf("%s%s%s%s\n", RED, "cd: no such file or directory: ",
 					command_line[1], RESET);
-		free(pwd_new);
-		ft_clean_2d_char(command_line);
+		ft_clean(envp);
 		return (0);
 	}
 }
@@ -60,26 +61,20 @@ void	tilda(char **command_line, char *pwd_new)
 		ft_strcat(pwd_new, command_line[1]);
 }
 
-char *get_global_var(char **envp, char *command_line) // test with setenv;
+char	*get_global_var(char **envp, char *command_line) // test with setenv;
 {
 	int	i;
 	char *buf;
 	char *result;
-	// char *find = ft_strdup(&command_line[1]);
-
-// ft_printf("find->%s\n", find);
 
 	i = 0;
 	buf = NULL;
 	result = NULL;
 	while (envp[i])
 	{
-		if (ft_strncmp(envp[i], command_line + 1, 6) == 0) /* = */
+		if (ft_strncmp(envp[i], &(command_line[1]), ft_strlen(&(command_line[1]))) == 0)
 		{
-			// ft_putstr("found\n");
 			buf = ft_strdup(envp[i]);
-			// ft_putstr(buf);
-			// ft_putstr("\n");
 			break;
 		}
 		i++;
@@ -89,15 +84,11 @@ char *get_global_var(char **envp, char *command_line) // test with setenv;
 	{
 		while (buf[i] && buf[i] != '=')
 			i++;
-		i++;
-		result = ft_strdup(&buf[i]);
-			// ft_printf("$->%s\n", result);
+		result = ft_strdup(&buf[++i]);
 		free(buf);
-		// free(find);
 	}
 	return (result);
 }
-
 
 short	one_and_too_many_argv(char **command_line, char *pwd_old, char **envp)
 {
@@ -105,8 +96,9 @@ short	one_and_too_many_argv(char **command_line, char *pwd_old, char **envp)
 
 	if (len_2d_array(command_line) == 1)
 	{
-		chdir("/Users/otimofie"); // make a var with homw variable !
-		envp[6] = "/Users/otimofie";
+		chdir("/Users/otimofie");
+		ft_clean(envp);
+		envp[6] = ft_strdup("/Users/otimofie");
 		envp[22] = ft_strdup(pwd_old);
 		ft_clean_2d_char(command_line);
 		return (0);
@@ -122,14 +114,10 @@ short	one_and_too_many_argv(char **command_line, char *pwd_old, char **envp)
 		path = get_global_var(envp, command_line[1]);
 		ft_printf("path->%s\n", path);
 		if (chdir(path) == 0)
-		{ // make a var with homw variable !
-			ft_putstr("Success\n");
-			if (envp[6])
-				free(envp[6]);
-				envp[6] = ft_strdup(path);
-			if (envp[22])
-				free(envp[22]);
-				envp[22] = ft_strdup(pwd_old);
+		{
+			ft_clean(envp);
+			envp[6] = ft_strdup(path);
+			envp[22] = ft_strdup(pwd_old);
 		}
 		if (path)
 			free(path);
@@ -153,14 +141,17 @@ void	cd(char *str, char **envp)
 	getcwd(&pwd_old[7], sizeof(pwd_old));
 	if (!one_and_too_many_argv(command_line, pwd_old, envp))
 		return ;
-	if (command_line[1][0] == '~')
-		len += ft_strlen("/Users/otimofie");
+	len = (command_line[1][0] == '~') ? len += ft_strlen("/Users/otimofie") : 0;
 	pwd_new = (char *)malloc(sizeof(char) * (ft_strlen(str) + 4 + 1 + len));
 	ft_memset(pwd_new, 0x0, sizeof(pwd_new));
 	ft_strcat(pwd_new, "PWD=");
 	tilda(command_line, pwd_new);
 	if (!cd_main(pwd_new, pwd_old, envp, command_line))
+	{
+		(pwd_new) ? free(pwd_new) : 0;
+		ft_clean_2d_char(command_line);
 		return ;
+	}
 	free(pwd_new);
 	ft_clean_2d_char(command_line);
 }
--- srcs/main.c
+++ srcs/main.c
@@ -27,8 +27,6 @@ int		main(int argc, char **argv, char **envp)
 
 	envp_buf = (char **)malloc(sizeof(char *) * (len + 1));
 
-
-
 	int i = 0;
 	
 	while(envp[i])
@@ -46,9 +44,7 @@ int		main(int argc, char **argv, char **envp)
 
 	envp_buf[len] = NULL;
 
-
-
-	while(get_next_line(0,&line))
+	while(get_next_line(0, &line))
 	{
 		if (ft_strstr(line, "cd"))
 			cd(line, envp_buf);
@@ -63,7 +59,7 @@ int		main(int argc, char **argv, char **envp)
 	}
 
 	system("leaks -q minishell");
-	ft_clean_2d_char(envp_buf);
+	// ft_clean_2d_char(envp_buf);
 	return 0;
 }
 
