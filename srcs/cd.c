b713e4c769954479f1e35d5d077c29502d9ec233
contrbas@gmail.com <K4rmU78B>
2018-11-24 16:15:25 +0200

    started to work on the cd function

--- srcs/cd.c
+++ srcs/cd.c
@@ -6,7 +6,7 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/24 13:28:48 by otimofie          #+#    #+#             */
-/*   Updated: 2018/11/24 16:11:22 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/11/24 16:14:41 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,25 +21,28 @@ void	cd(char *str, char **envp)
 	char pwd_old[1024];
 	char *pwd_new;
 
-	command_line = ft_strsplit(str, 32); // not one argument;
-
+	command_line = ft_strsplit(str, 32);
 	ft_memset(pwd_old, 0x0, sizeof(pwd_old));
 	ft_strcat(pwd_old, "OLDPWD=");
-
-	getcwd(&pwd_old[7], sizeof(pwd_old));
-	
+	getcwd(&pwd_old[7], sizeof(pwd_old));	
 	pwd_new = (char *)malloc(sizeof(char) * (ft_strlen(str) + 5));
 	ft_memset(pwd_new, 0x0, sizeof(pwd_new));
 	ft_strcat(pwd_new, "PWD=");
 	ft_strcat(pwd_new, command_line[1]);
 
-
+	// separate func;
 	if (chdir(command_line[1]) == 0)
 	{
 		envp[6] = pwd_new;
 		envp[22] = pwd_old;
 		ft_printf("%s\n", "dir has been changed");
 	}
+	else
+	{
+		ft_printf("%s%s\n", "cd: no such file or directory: ", command_line[1]);
+		exit(0);
+	}
+
 	
 	// else
 	// {
