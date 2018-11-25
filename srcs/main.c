67e2400a41622c4728ad988d1dd6c7896dd3fc4b
contrbas@gmail.com <K4rmU78B>
2018-11-25 13:56:25 +0200

    added init func for envp_buf

--- srcs/main.c
+++ srcs/main.c
@@ -24,9 +24,10 @@ int		main(int argc, char **argv, char **envp)
 			env_minishell(envp_buf);
 		else if (ft_strstr(line, "exit"))
 			exit_minishell(envp_buf);
-		else
-			ft_printf("%sminishell: command not found: %s%s\n", 
-				RED, line, RESET);
+		// else
+			// ft_printf("%sminishell: command not found: %s%s\n", 
+			// 	RED, line, RESET);
+			// use fork;
 		system("leaks -q minishell");
 		free(line);
 	}
