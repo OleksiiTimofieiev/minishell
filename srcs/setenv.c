ca4a78de91d982c29a4c71e0fb319f655d29cf05
contrbas@gmail.com <K4rmU78B>
2018-11-25 16:21:54 +0200

    started setenv

--- srcs/setenv.c
+++ srcs/setenv.c
@@ -6,7 +6,7 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/25 15:20:20 by otimofie          #+#    #+#             */
-/*   Updated: 2018/11/25 15:53:29 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/11/25 16:21:49 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,62 @@ int		get_index_envp(char **envp, char *name)
 	return (0);
 }
 
-int setenv_minishell(char *str, char **envp)
+char	**get_buf_envp(char **arguments, char **envp)
 {
-	// const char *name, const char *value, int overwrite,
+	int i;
+	int len;
+	char **buf;
+
+	i = 0;
+	len = len_char_2d_array(envp);
+	buf = (char **)malloc(sizeof(char *) * (len + 2));
+	while(envp[i])
+	{
+		buf[i] = ft_strdup(envp[i]);
+		i++;
+	}
+	buf[i] = ft_strdup(arguments[1]);
+	ft_strcat(buf[i], "=");
+	ft_strcat(buf[i], arguments[2]);
+	ft_printf("argument->%s\n", buf[i]);
+	buf[len + 1] = NULL;
+	return (buf);
+}
+
+void	realloc_envp(char **envp, char **buf)
+{
+	int i;
+	int len;
+
+	i = 0;
+	len = len_char_2d_array(envp) + 1;
+	envp = (char **)malloc(sizeof(char *) * len);
+
+	while(buf[i])
+	{
+		envp[i] = ft_strdup(buf[i]);
+		i++;
+	}
+	envp[i] = NULL;
+}
+
+int		setenv_minishell(char *str, char **envp)
+{
+
 	int i;
 	char **arguments;
+	int	len;
+	char **envp_buf;
 
 	arguments = ft_strsplit(str, 32);
-	if (len_char_2d_array(arguments) < 3)
+	len = len_char_2d_array(arguments);
+
+	if (len < 4 || len > 4)
+	{
+		ft_clean_2d_char(arguments);
+
 		return (0);
-	
+	}
 
 	i = get_index_envp(envp, arguments[1]);
 	if (i)
@@ -54,6 +100,12 @@ int setenv_minishell(char *str, char **envp)
 	else
 	{
 		ft_printf("index not found -> %d\n", i);
+		envp_buf = get_buf_envp(arguments, envp);
+		ft_clean_2d_char(envp);
+		realloc_envp(envp, envp_buf);
+		ft_clean_2d_char(envp_buf);
 	}
+	ft_clean_2d_char(arguments);
+
 	return (0);
 }
