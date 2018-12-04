8f78e918a31507c13ae153428bcda0fe43b10813
Oleksii Timofieiev <otimofie@Oleksiis-MacBook-Pro.local>
2018-12-04 19:47:28 +0200

    some ideas on fork

--- srcs/main.c
+++ srcs/main.c
@@ -24,8 +24,10 @@
 // TODO: unsetenv value is not in the env list , NAME=Value format, arguments is more then 2;
 // TODO: try getcwd with cd to get more cool cd
 
+// find in PATH variable;
 // check if the command available -> split path by : check that the file is binary one and access rights;
 // if available ->strdup
+
 int lsh_launch(char **env_array)
 {
    pid_t pid, wpid;
