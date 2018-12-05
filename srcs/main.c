a46dfccf8c1438f46bf7f8787de50475b6a57900
Oleksii Timofieiev <otimofie@Oleksiis-MacBook-Pro.local>
2018-12-05 19:39:18 +0200

    noeminette stuff

--- srcs/main.c
+++ srcs/main.c
@@ -19,26 +19,18 @@
 // TODO: validation for setenv (quantity of arguments, 1 or 0)
 // TODO: validation for main
 // TODO: quotes -> dismiss quotes;
-// TODO: maybe: cfunc to detect the type of the comd (if true -> continue);
+// TODO: maybe: cfunc to detect the type of the cmd (if true -> continue);
 // TODO: work out when there is no such env var in unsetenv
 // TODO: unsetenv value is not in the env list , NAME=Value format, arguments is more then 2;
 // TODO: try getcwd with cd to get more cool cd
+// TODO: if line == NULL;
 
-int		main(int argc, char **argv, char **envp)
+void	main_routine(char *line, char **envp_buf)
 {
-	(void)argc;
-	(void)argv;
-	char *line;
-	char **envp_buf;
-
-	envp_buf = init_envp_buf(envp);
-
 	while (1)
 	{
 		ft_printf("%s%s%s", GREEN, "$> ", RESET);
-
 		get_next_line(0, &line);
-
 		if (!ft_strncmp(line, "cd", 2))
 			cd(line, envp_buf);
 		else if (!ft_strncmp(line, "echo", 4))
@@ -51,20 +43,26 @@ int		main(int argc, char **argv, char **envp)
 			envp_buf = setenv_minishell(line, envp_buf);
 		else if (!ft_strncmp(line, "unsetenv", 8))
 			envp_buf = unsetenv_minishell(line, envp_buf);
-
-		run_buitin_cmd(envp);
-		system("leaks -q minishell");
-
-		
-		// else
-			// ft_printf("%sminishell: command not found: %s%s\n", 
-			// 	RED, line, RESET);
-			// use fork;
-
+		else
+			run_buitin_cmd(envp_buf);
 		free(line);
-
-
-	}
 		
+		system("leaks -q minishell");
+	}
+}
+
+int		main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	char *line;
+	char **envp_buf;
+
+	line = NULL;
+	envp_buf = init_envp_buf(envp);
+
+	main_routine(line, envp_buf);
+
+
 	return 0;
 }
