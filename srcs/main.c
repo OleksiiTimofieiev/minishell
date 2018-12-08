8d2b8d5eef83e6387ad4e0ce906e98b59fdaf5e8
contrbas@gmail.com <K4rmU78B>
2018-12-08 18:51:49 +0200

    now PWD && OLDPWD are at random place

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/01 15:35:06 by otimofie          #+#    #+#             */
-/*   Updated: 2018/12/08 17:48:35 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/12/08 18:51:27 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,23 @@
 // TODO: add var if $ or ~ to the line, rethink the funcs;
 
 // TODO: quotes "" ''
+
+int detect_del_var_main(char *env, char **haystack)
+{
+	int i;
+	int len_of_env;
+
+	i = 0;
+	len_of_env = ft_strlen(env);
+	while (haystack[i])
+	{
+		if (ft_strncmp(env, haystack[i], len_of_env) == 0 && haystack[i][len_of_env] == '=') //
+			return (i);
+		i++;
+	}
+	return ('x');
+}
+
 
 void	minishell(char **envp_in)
 {
@@ -35,12 +52,20 @@ void	minishell(char **envp_in)
 
 	while (1)
 	{
+		if (detect_del_var_main("OLDPWD", envp_buf) == 'x')
+			envp_buf = setenv_minishell("setenv OLDPWD /Users/otimofie 1", envp_buf);
+		else if (detect_del_var_main("PWD", envp_buf) == 'x')
+			envp_buf = setenv_minishell("setenv PWD /Users/otimofie 1", envp_buf);
+		// else if (detect_del_var_main("HOME", envp_buf) == 'x') / wtf ?
+		// 	envp_buf = setenv_minishell("setenv HOME /Users/otimofie 1", envp_buf);
+
 		len_env_vars = ft_2d_arr_size(envp_buf) - 1;
 
 		ft_printf("%s%s%s", GREEN, "$> ", RESET);
 		if (!(get_next_line(0, &line)))
 			exit (0);
 		len_env_vars = ft_2d_arr_size(envp_buf) - 1;
+
 
 		if (!ft_strncmp(line, "cd", 2))
 			cd(line, envp_buf);
