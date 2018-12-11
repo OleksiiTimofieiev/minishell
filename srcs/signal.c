edd7ee2e7775fa385b17820617627d356cc69b5e
OleksiiTimofieiev <contrbas@gmail.com>
2018-12-11 19:04:25 +0200

    signal handler

--- srcs/signal.c
+++ srcs/signal.c
@@ -7,7 +7,7 @@ void	signal_handler(int sig_num)
     /*do something*/
     system("clear");
     ft_printf("%sMagic... moments...%s\n", CYAN, RESET);
-    ft_printf("Please enter the command\n");
+    ft_printf("%s%s%s", GREEN, "$> ", RESET);
     // sig_num = 'c';
     // exit(0);signal(SIGINT, signal_handler);
     signal(SIGINT, signal_handler);
