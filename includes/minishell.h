eda1b23494cd7040eb806c297fcf0b8eeea4b382
otimofie <contrbas@gmail.com>
2018-12-23 15:24:33 +0200

    started with fork flags

--- includes/minishell.h
+++ includes/minishell.h
@@ -18,27 +18,27 @@ typedef struct      s_env
 **************************** core ******************************
 */
 
-void	echo(char *str, t_env **env);
-void	cd(char *str, t_env **env);
-void	exit_minishell(t_env **env);
-void 	env_minishell(t_env *env);
-void	signal_handler(int sig_num);
-void	run_buitin_cmd(char *line, t_env *env);
-void	setenv_minishell(char *str, t_env *env);
-void	unsetenv_minishell(char *str, t_env *env);
+void				echo(char *str, t_env **env);
+void				cd(char *str, t_env **env);
+void				exit_minishell(t_env **env);
+void				env_minishell(t_env *env);
+void				signal_handler(int sig_num);
+void				run_buitin_cmd(char *line, t_env *env);
+void				setenv_minishell(char *str, t_env *env);
+void				unsetenv_minishell(char *str, t_env *env);
 
 /*
 **************************** infrastructure ********************
 */
 
-void    push_back(t_env **head, char *str);
-void	init_env(t_env **env);
-void	ft_clean(t_env **env);
-int		detect_del_var(char *env, char **haystack);
-short	one_and_too_many_argv(char **command_line, char *pwd_old, t_env **env);
-t_env   *find_elem(t_env **env, char *str);
-int		env_var_detection(char *str);
-int		spaces_quantity(char **arguments, int start_index);
-char	*find_binary_path(char *binary_name, t_env **env_array);
+void				push_back(t_env **head, char *str);
+void				init_env(t_env **env);
+void				ft_clean(t_env **env);
+int					detect_del_var(char *env, char **haystack);
+short				one_and_too_many_argv(char **command_line, char *pwd_old, t_env **env);
+t_env				*find_elem(t_env **env, char *str);
+int					env_var_detection(char *str);
+int					spaces_quantity(char **arguments, int start_index);
+char				*find_binary_path(char *binary_name, t_env **env_array);
 
 #endif
