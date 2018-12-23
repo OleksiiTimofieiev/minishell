ca6f0aa9e22da498ee60a53a200a43cafdbf08a2
otimofie <contrbas@gmail.com>
2018-12-23 10:10:57 +0200

    started working on setenv

--- srcs/env_infrastructure.c
+++ srcs/env_infrastructure.c
@@ -18,10 +18,7 @@ t_env   *find_elem(t_env **head, char *str)
     while (current)
     {
         if (ft_strcmp( current->name, str) == 0)
-        {
-        	// ft_putstr("asdf\n");
             return (current);
-        }
         current = current->next;
     }
     return (NULL);
@@ -29,46 +26,15 @@ t_env   *find_elem(t_env **head, char *str)
 
 void	ft_clean(t_env **env)
 {
-	// t_env **path =(t_env **)malloc(sizeof(t_env *) * 2);
-
 	t_env *path_x;
 	t_env *path_y;
 
-
 	path_x = find_elem(env, "PWD");
 	path_y = find_elem(env, "OLDPWD");
-
-			ft_printf("%s%s\n%s", CYAN, path_x->content, RESET);
-
-
-
-
 	if (path_x->content != NULL)
 	{
 		free(path_x->content);
 	}
 	if (path_y->content != NULL)
 		free(path_y->content);
-	
-	ft_printf("%s%s\n%s", CYAN, "6", RESET);
 }
-
-
-// int		detect_del_var(char *env, char **haystack)
-// {
-// 	int i;
-// 	int len_of_env;
-
-// 	i = 0;
-// 	len_of_env = ft_strlen(env);
-// 	while (haystack[i])
-// 	{
-// 		if (ft_strncmp(env, haystack[i], len_of_env) == 0)
-// 		{
-// 			if (haystack[i][len_of_env] == '=')
-// 				return (i);
-// 		}
-// 		i++;
-// 	}
-// 	return ('x');
-// }
